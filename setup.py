8fd666253fa84ce3171f14ec7d2137fda32ee1d0
SuryaSankar <suryashankar.m@gmail.com>
2015-05-03 01:51:25 +0530

    removing files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='schemalite',
+    version='0.1.0',
+    long_description='A minimalistic Schema validation library',
+    zip_safe=False,
+    license='MIT',
+    author='SuryaSankar',
+    author_email='surya@inkmonk.com')
