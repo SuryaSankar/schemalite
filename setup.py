512365111ef77a51359abd1f8ccfeae9370fda4a
Surya Sankar <suryashankar.m@gmail.com>
2018-05-22 10:49:54 +0530

    adding toolspy dependency

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup
 
 setup(
     name='schemalite',
-    version='0.1.22',
+    version='0.1.23',
     description='A minimalistic Schema validation library',
     long_description='A minimalistic Schema validation library',
     packages=['schemalite'],
     include_package_data=True,
     license='MIT',
+    install_requires=[
+        "toolspy>=0.2.25"
+    ],
     author='SuryaSankar',
     author_email='suryashankar.m@gmail.com')
