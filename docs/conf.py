0e7d9209caeb637b04faa995e4d8eac360434b30
Surya Sankar <suryashankar.m@gmail.com>
2019-04-11 19:18:04 +0530

    adding read the docs yml

--- docs/conf.py
+++ docs/conf.py
@@ -18,11 +18,11 @@
 # relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
 #
+import schemalite
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import schemalite
 
 # -- General configuration ---------------------------------------------
 
@@ -32,7 +32,8 @@ import schemalite
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.viewcode', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -158,6 +159,3 @@ texinfo_documents = [
      'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
