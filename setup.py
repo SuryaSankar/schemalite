53c68dd42ae252763cd60b7c46a93c060280239d
SuryaSankar <suryashankar.m@gmail.com>
2019-06-19 11:32:16 +0530

    modifying long description to use readme

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     description="A minimal, yet powerful schema validation library for python",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='schemalite',
     name='schemalite',
